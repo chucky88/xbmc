3d3a413f7669861a71327741a4817658733ba32c
cpm <turbine_rally0c@icloud.com>
2024-06-15 12:20:50 +0000

    DV Use aml_dv_mode()
    DV use aml_dv_mode
    
    DV Remove unnecessary check for dv support as encapsulated in the DV_MODE

--- xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/RendererAML.cpp
+++ xbmc/cores/VideoPlayer/VideoRenderers/HwDecRender/RendererAML.cpp
@@ -64,8 +64,6 @@ bool CRendererAML::Configure(const VideoPicture &picture, float fps, unsigned in
   SetViewMode(m_videoSettings.m_ViewMode);
   ManageRenderArea();
 
-  auto settings = CServiceBroker::GetSettingsComponent()->GetSettings();
-
   // Configure GUI/OSD for HDR PQ when display is in HDR PQ mode
   bool hdr_display(CServiceBroker::GetWinSystem()->IsHDRDisplay() || aml_display_support_dv());
   bool dv_on(aml_dv_mode() != DV_MODE_OFF);
